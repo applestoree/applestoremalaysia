068e185a6ff312b8f0db2bbdf4d5334216663e08
applestoree <applestoremalaysiaa@gmail.com>
2026-09-12 13:49:14 +0800

    Update Header logo and remove text title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+const APP_LOGO = 'https://res.cloudinary.com/daj5cu840/image/upload/v1789185480/New_Project_8_bmwtnj_yq0qjf.png'
+
 export default function Header({ onCart, cartCount }) {
   return (
     <header className="header">
-      <div className="logo">Apple Store Malaysia</div>
+      <img className="logo-image" src={APP_LOGO} alt="Apple Store Malaysia" />
       <div className="header-actions">
         <button className="icon-button" aria-label="Notifications">
           ♡
